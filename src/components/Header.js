dcaf569a19481bf836950e34fbb93d24e731e36a
Shahina4 <90304923+Shahina4@users.noreply.github.com>
2025-06-10 16:07:40 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap';
+
+const Header = () => {
+    return(
+        <header>
+            <Navbar bg="lightdark" expand="lg">
+
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
