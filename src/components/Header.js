b55afd46d946293b840d9c4ea233fe03d0a11db4
Shahina4 <90304923+Shahina4@users.noreply.github.com>
2025-06-13 16:42:20 +0530

    Added Images folder, created Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,42 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap';
-
+import {Navbar, Nav, NavDropdown, Container, Image} from 'react-bootstrap';
 const Header = () => {
     return(
         <header>
-            <Navbar bg="lightdark" expand="lg">
+            <Navbar className="navbar-style" bg="lightdark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#">
+                        <Image className="logo-img-style" src='images/logo.jpg' roundedCircle/>
+                        <h6 className="logo-text-style" >Visvesvaraya Technological University, Belgaum.</h6>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#">Home</Nav.Link>
+                            <Nav.Link href="#link">About us</Nav.Link>
+                            <NavDropdown title="Academic" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#">Admission</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Circulars & Notification</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Certificate Issued</NavDropdown.Item>
+                            </NavDropdown>
 
+                            <NavDropdown title="Examination" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#">Examination Guidelines</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Examination Application</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Results</NavDropdown.Item>
+                            </NavDropdown>
+
+                            <NavDropdown title="VTU Departments" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#">Civil Engg</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Computer Science Engg</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Electronics & Communication Engg</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Staff Login" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#">Login as Admin</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </header>
     )
